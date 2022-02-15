80d9d74aa8fa3fc322bc04e5124dd4e448cb586c
Kimiaki Kuno <knokmki612@gmail.com>
2022-02-15 19:08:52 +0900

    chore: setup express as server middleware

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,7 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  serverMiddleware: [
+    { path: '/express', handler: '~/express/index.js' }
+  ]
 })
