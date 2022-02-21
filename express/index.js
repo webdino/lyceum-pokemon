f772716c87fed32d455696826b902281192e1846
Kimiaki Kuno <knokmki612@gmail.com>
2022-02-21 11:38:29 +0900

    refactor: APIのCRUD処理と使用する環境変数の外部化

--- express/index.js
+++ express/index.js
@@ -1,20 +1,11 @@
 import express from "express";
 import {
-  ListObjectsCommand,
-  GetObjectCommand,
-  PutObjectCommand,
-  DeleteObjectCommand,
-} from "@aws-sdk/client-s3";
-import s3Client from "./utils/s3Client";
-import { BUCKET_NAME } from "./utils/env";
-
-const streamToString = (stream) =>
-  new Promise((resolve, reject) => {
-    const chunks = [];
-    stream.on("data", (chunk) => chunks.push(chunk));
-    stream.on("error", reject);
-    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
-  });
+  findTrainers,
+  findTrainer,
+  upsertTrainer,
+  deleteTrainer,
+} from "./utils/trainer";
+import { findPokemon } from "./utils/pokemon";
 
 const app = express();
 
@@ -28,10 +19,8 @@ app.get("/", (req, res) => {
 /** トレーナーの一覧の取得 */
 app.get("/trainers", async (req, res) => {
   try {
-    const objects = await s3Client.send(
-      new ListObjectsCommand({ Bucket: BUCKET_NAME })
-    );
-    res.send(objects.Contents);
+    const trainers = await findTrainers();
+    res.send(trainers);
   } catch (err) {
     res.status(500).send(err);
   }
@@ -40,14 +29,9 @@ app.get("/trainers", async (req, res) => {
 /** トレーナーの取得 */
 app.get("/trainer/:trainerName", async (req, res) => {
   try {
-    const object = await s3Client.send(
-      new GetObjectCommand({
-        Bucket: BUCKET_NAME,
-        Key: `${req.params.trainerName}.json`,
-      })
-    );
-    const body = await streamToString(object.Body);
-    res.type("application/json").send(body);
+    const { trainerName } = req.params;
+    const trainer = await findTrainer(trainerName);
+    res.send(trainer);
   } catch (err) {
     res.status(500).send(err);
   }
@@ -56,13 +40,7 @@ app.get("/trainer/:trainerName", async (req, res) => {
 /** トレーナーの追加更新 */
 app.post("/trainer", async (req, res) => {
   try {
-    const result = await s3Client.send(
-      new PutObjectCommand({
-        Bucket: BUCKET_NAME,
-        Key: `${req.body.name}.json`,
-        Body: JSON.stringify({ pokemons: req.body?.pokemons ?? [] }),
-      })
-    );
+    const result = await upsertTrainer(req.body.name, req.body);
     res.status(result["$metadata"].httpStatusCode).send(result);
   } catch (err) {
     res.status(500).send(err);
@@ -72,12 +50,8 @@ app.post("/trainer", async (req, res) => {
 /** トレーナーの削除 */
 app.delete("/trainer/:trainerName", async (req, res) => {
   try {
-    const result = await s3Client.send(
-      new DeleteObjectCommand({
-        Bucket: BUCKET_NAME,
-        Key: `${req.params.trainerName}.json`,
-      })
-    );
+    const { trainerName } = req.params;
+    const result = await deleteTrainer(trainerName);
     res.status(result["$metadata"].httpStatusCode).send(result);
   } catch (err) {
     res.status(500).send(err);
@@ -85,68 +59,50 @@ app.delete("/trainer/:trainerName", async (req, res) => {
 });
 
 /** ポケモンの追加 */
-app.put("/trainer/:trainerName/pokemon/:pokemonOrder", async (req, res) => {
-  try {
-    const object = await s3Client.send(
-      new GetObjectCommand({
-        Bucket: BUCKET_NAME,
-        Key: `${req.params.trainerName}.json`,
-      })
-    );
-    const trainer = JSON.parse(await streamToString(object.Body));
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${req.params.pokemonOrder}`
-    );
-    const {
-      order,
-      name,
-      sprites: { front_default },
-    } = await response.json();
-    trainer.pokemons.push({
-      id: trainer.pokemons.length + 2,
-      nickname: "",
-      order,
-      name,
-      sprites: { front_default },
-    });
-    const result = await s3Client.send(
-      new PutObjectCommand({
-        Bucket: BUCKET_NAME,
-        Key: `${req.params.trainerName}.json`,
-        Body: JSON.stringify(trainer),
-      })
-    );
-    res.status(result["$metadata"].httpStatusCode).send(result);
-  } catch (err) {
+app.put(
+  "/trainer/:trainerName/pokemon/:pokemonOrder",
+  async (req, res) => {
+    try {
+      const { trainerName, pokemonOrder } = req.params;
+      const trainer = await findTrainer(trainerName);
+      const pokemon = await findPokemon(pokemonOrder);
+      const {
+        order,
+        name,
+        sprites: { front_default },
+      } = pokemon;
+      trainer.pokemons.push({
+        id: trainer.pokemons.length + 2,
+        nickname: "",
+        order,
+        name,
+        sprites: { front_default },
+      });
+      const result = await upsertTrainer(trainerName, trainer);
+      res.status(result["$metadata"].httpStatusCode).send(result);
+    } catch (err) {
     res.status(500).send(err);
+    }
   }
-});
+);
 
 /** ポケモンの削除 */
-app.delete("/trainer/:trainerName/pokemon/:pokemonId", async (req, res) => {
-  try {
-    const object = await s3Client.send(
-      new GetObjectCommand({
-        Bucket: BUCKET_NAME,
-        Key: `${req.params.trainerName}.json`,
-      })
-    );
-    const trainer = JSON.parse(await streamToString(object.Body));
-    const index = trainer.pokemons.findIndex(
-      (pokemon) => pokemon.id === req.params.pokemonId
-    );
-    trainer.pokemons.splice(index, 1);
-    const result = await s3Client.send(
-      new PutObjectCommand({
-        Bucket: BUCKET_NAME,
-        Key: `${req.params.trainerName}.json`,
-        Body: JSON.stringify(trainer),
-      })
-    );
-    res.status(result["$metadata"].httpStatusCode).send(result);
-  } catch (err) {
-    res.status(500).send(err);
+app.delete(
+  "/trainer/:trainerName/pokemon/:pokemonId",
+  async (req, res) => {
+    try {
+      const { trainerName, pokemonId } = req.params;
+      const trainer = await findTrainer(trainerName);
+      const index = trainer.pokemons.findIndex(
+        (pokemon) => pokemon.id === pokemonId
+      );
+      trainer.pokemons.splice(index, 1);
+      const result = await upsertTrainer(trainerName, trainer);
+      res.status(result["$metadata"].httpStatusCode).send(result);
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
-});
+);
 
 export default app;
---
+++ express/utils/trainer.js
@@ -0,0 +1,59 @@
+import {
+  ListObjectsCommand,
+  GetObjectCommand,
+  PutObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import s3Client from "./s3Client";
+import { BUCKET_NAME } from "./env";
+
+const streamToString = (stream) =>
+  new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on("data", (chunk) => chunks.push(chunk));
+    stream.on("error", reject);
+    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+  });
+
+/** トレーナーの一覧の取得 */
+export const findTrainers = async () => {
+  const objects = await s3Client.send(
+    new ListObjectsCommand({ Bucket: BUCKET_NAME })
+  );
+  return objects.Contents;
+};
+
+/** トレーナーの取得 */
+export const findTrainer = async (name) => {
+  const object = await s3Client.send(
+    new GetObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: `${name}.json`,
+    })
+  );
+  const trainer = JSON.parse(await streamToString(object.Body));
+  return trainer;
+};
+
+/** トレーナーの追加更新 */
+export const upsertTrainer = async (name, trainer) => {
+  const result = await s3Client.send(
+    new PutObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: `${name}.json`,
+      Body: JSON.stringify({ name: "", pokemons: [], ...trainer }),
+    })
+  );
+  return result;
+};
+
+/** トレーナーの削除 */
+export const deleteTrainer = async (name) => {
+  const result = await s3Client.send(
+    new DeleteObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: `${name}.json`,
+    })
+  );
+  return result;
+};
