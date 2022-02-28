94219d9b0c92ce02a3e65466c363c7a21f446c63
Kimiaki Kuno <knokmki612@gmail.com>
2022-02-28 13:40:38 +0900

    docs: サーバミドルウェア

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@ import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  /**
+   * NOTE: Express を Nuxt のサーバミドルウェアとして導入
+   * https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#servermiddleware
+   */
   serverMiddleware: [{ path: "/express", handler: "~/express/index.js" }],
 });
