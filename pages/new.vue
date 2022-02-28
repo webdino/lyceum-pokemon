ea6f141f2330027762ecc4d56ca8c44f15e7776a
Kimiaki Kuno <knokmki612@gmail.com>
2022-02-28 12:32:49 +0900

    feat: トレーナーを追加するために必要な入力フォーム

--- pages/new.vue
+++ pages/new.vue
@@ -1,3 +1,35 @@
+<script>
+export default {
+  setup() {
+    const router = useRouter()
+    const trainerName = ref("")
+    const onSubmit = async () => {
+      const response = await fetch("http://localhost:3000/express/trainer", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name: trainerName.value
+        })
+      })
+      if (!response.ok) return
+      router.push(`/trainer/${trainerName.value}`)
+    }
+    return {
+      trainerName,
+      onSubmit
+    }
+  }
+}
+</script>
+
 <template>
-  <p>TODO: 必要なフォームの作成</p>
+  <form @submit.prevent="onSubmit">
+    <label>
+      なまえ
+      <input v-model="trainerName" />
+    </label>
+    <button type="button" @click="onSubmit">はじめる</button>
+  </form>
 </template>
