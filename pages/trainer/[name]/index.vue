b6a8a8715a1c5b3aee033722b93ae58cc07bc676
Kimiaki Kuno <knokmki612@gmail.com>
2022-02-28 12:36:08 +0900

    feat: 見出しの調整

--- pages/trainer/[name]/index.vue
+++ pages/trainer/[name]/index.vue
@@ -26,7 +26,7 @@ export default {
 <template>
   <div>
     <p>トレーナー: {{trainer.name}}</p>
-    <h3>手持ちポケモン</h3>
+    <h3>てもちポケモン</h3>
     <ul>
       <li v-for="pokemon in trainer.pokemons" :id="pokemon.id">
         <img :src="pokemon.sprites.front_default" />
