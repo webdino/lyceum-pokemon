553bd38a9645d99d80811dab6b7f6720ad0be405
Kimiaki Kuno <knokmki612@gmail.com>
2022-03-02 22:07:02 +0900

    feat(GamifyList): 枠線の有無、並ぶ方向のプロパティ

--- components/GamifyList.vue
+++ components/GamifyList.vue
@@ -1,5 +1,20 @@
+<script>
+export default {
+  props: {
+    border: {
+      default: true,
+      type: Boolean
+    },
+    direction: {
+      default: "vertical",
+      type: String
+    }
+  }
+}
+</script>
+
 <template>
-  <ul class="gamify-list">
+  <ul class="gamify-list" :class="{border, horizon: direction === 'horizon'}">
     <slot />
   </ul>
 </template>
@@ -7,8 +22,19 @@
 <style scoped>
 .gamify-list {
   list-style-type: none;
+}
+
+.gamify-list.border {
   border-radius: 0.5rem;
   border: solid 4px #555;
   padding: 1.5rem 3rem;
+}
+
+.gamify-list.horizon {
+  display: flex;
+}
+
+.gamify-list.horizon > :not(:last-child) {
+  margin-right: 0.5rem;
 }
 </style>
