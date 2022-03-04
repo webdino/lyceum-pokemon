786dbaa6740593f39251700161d72faa9b2320a5
Hidetaro7 <hidetaro7@gmail.com>
2022-03-04 17:26:12 +0900

    feat: GamifyButtonコンポーネントの作成

---
+++ components/GamifyButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="buttonClicked()">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  setup(props, { emit }) {
+    const buttonClicked = () => {
+      emit("click-button");
+    };
+    return {
+      buttonClicked,
+    };
+  },
+};
+</script>
