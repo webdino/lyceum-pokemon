93bd2740ff771a8216d798aa4fd1bcbc74555242
Kimiaki Kuno <knokmki612@gmail.com>
2022-08-17 22:07:30 +0900

    fix: nuxi dev 時正常にサーバが起動しない
    動作が確認できていたバージョンのnuxt3に戻すことで暫定的に対処

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
