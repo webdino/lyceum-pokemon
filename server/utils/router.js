2327517a642655f5bda1be5f83804c44dad4cdf0
Kimiaki Kuno <knokmki612@gmail.com>
2024-01-24 17:21:00 +0900

    docs: typo

--- server/utils/router.js
+++ server/utils/router.js
@@ -53,7 +53,7 @@ router.post("/trainer/:trainerName", async (req, res, next) => {
 router.post("/trainer/:trainerName/pokemon", async (req, res, next) => {
   try {
     const { trainerName } = req.params;
-    // TODO: リクエストボディにトレーナー名が含まれていなければ400を返す
+    // TODO: リクエストボディにポケモン名が含まれていなければ400を返す
     const pokemon = await findPokemon(req.body.name);
     // TODO: 削除系 API エンドポイントを利用しないかぎりポケモンは保持する
     const result = await upsertTrainer(trainerName, { pokemons: [pokemon] });
