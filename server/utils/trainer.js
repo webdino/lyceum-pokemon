0b6252b8fa43e86e1144b1df0531a234f75ec543
Kimiaki Kuno <knokmki612@gmail.com>
2025-01-24 19:00:31 +0900

    S3Client のプロキシーネットワーク環境での動作を考慮する

--- server/utils/trainer.js
+++ server/utils/trainer.js
@@ -5,9 +5,17 @@ import {
   PutObjectCommand,
   S3Client,
 } from "@aws-sdk/client-s3";
+import { NodeHttpHandler } from "@aws-sdk/node-http-handler";
+import { ProxyAgent } from "proxy-agent";
 
 const config = useRuntimeConfig();
-const s3Client = new S3Client({ region: config.region });
+
+const agent = new ProxyAgent();
+
+const s3Client = new S3Client({
+  region: config.region,
+  requestHandler: new NodeHttpHandler({ httpAgent: agent, httpsAgent: agent }),
+});
 
 const streamToString = (stream) =>
   new Promise((resolve, reject) => {
