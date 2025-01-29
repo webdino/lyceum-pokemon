7aa7b08057bafb3c0e1596b8c338ceae076866f8
Kimiaki Kuno <knokmki612@gmail.com>
2025-01-29 11:57:16 +0900

    chore(deps): {@aws-sdk,@smithy}/node-http-handler (#169)

--- server/utils/trainer.js
+++ server/utils/trainer.js
@@ -5,7 +5,7 @@ import {
   PutObjectCommand,
   S3Client,
 } from "@aws-sdk/client-s3";
-import { NodeHttpHandler } from "@aws-sdk/node-http-handler";
+import { NodeHttpHandler } from "@smithy/node-http-handler";
 import { ProxyAgent } from "proxy-agent";
 
 const config = useRuntimeConfig();
