7216f7990f06fec3e2e4c41ffacd5ffeff06add6
Kimiaki Kuno <knokmki612@gmail.com>
2025-02-05 11:00:38 +0900

    ofetch でのプロキシーエージェントの追加方法の見直し
    ofetch の agent オプションは Node v18 未満で動作するみたい
    Node.js のバージョンをチェックし、Undici Dispatcher API を使用する形に見直します
    
    see https://github.com/unjs/ofetch/blob/main/README.md#%EF%B8%8F-adding-https-agent

--- server/utils/pokemon.js
+++ server/utils/pokemon.js
@@ -1,11 +1,28 @@
-import { ProxyAgent } from "proxy-agent";
 import { ofetch } from "ofetch";
 
-const agent = new ProxyAgent();
+const agentFactory = async () => {
+  if (!(process.env.HTTPS_PROXY || process.env.HTTP_PROXY)) return {};
+  const nodeVer = process.version.match(
+    /^v(?<major>[0-9]+)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)/,
+  );
+  const nodeMajorVer = Number(nodeVer.groups.major);
+  if (nodeMajorVer < 18) {
+    const { ProxyAgent } = await import("proxy-agent");
+    const agent = new ProxyAgent();
+    return { agent };
+  }
+  const { ProxyAgent } = await import("undici");
+  const dispatcher = new ProxyAgent(
+    process.env.HTTPS_PROXY ?? process.env.HTTP_PROXY,
+  );
+  return { dispatcher };
+};
+
 /** ポケモンの取得 */
 export const findPokemon = async (name) => {
-  const pokemon = await ofetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-    agent,
-  });
+  const pokemon = await ofetch(
+    `https://pokeapi.co/api/v2/pokemon/${name}`,
+    await agentFactory(),
+  );
   return pokemon;
 };
