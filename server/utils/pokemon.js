597c5b307b971f8d57e91da71f46c73ab4834f09
Kimiaki Kuno <knokmki612@gmail.com>
2025-02-05 16:01:16 +0900

    NOTE コメント

--- server/utils/pokemon.js
+++ server/utils/pokemon.js
@@ -1,6 +1,8 @@
 import { ofetch } from "ofetch";
 import { EnvHttpProxyAgent } from "undici";
 
+// NOTE: Warning: EnvHttpProxyAgent is experimental, expect them to change at any time
+// See https://undici.nodejs.org/#/docs/api/EnvHttpProxyAgent.md
 const envHttpProxyAgent = new EnvHttpProxyAgent();
 
 /** ポケモンの取得 */
