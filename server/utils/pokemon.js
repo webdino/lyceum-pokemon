dfd149cd204bec7da83ecc900b9d2562e4ee205f
Kimiaki Kuno <knokmki612@gmail.com>
2025-02-05 11:09:47 +0900

    dispatcher オプションのみ使う

--- server/utils/pokemon.js
+++ server/utils/pokemon.js
@@ -1,28 +1,18 @@
 import { ofetch } from "ofetch";
 
-const agentFactory = async () => {
-  if (!(process.env.HTTPS_PROXY || process.env.HTTP_PROXY)) return {};
-  const nodeVer = process.version.match(
-    /^v(?<major>[0-9]+)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)/,
-  );
-  const nodeMajorVer = Number(nodeVer.groups.major);
-  if (nodeMajorVer < 18) {
-    const { ProxyAgent } = await import("proxy-agent");
-    const agent = new ProxyAgent();
-    return { agent };
-  }
+const createProxyAgent = async () => {
+  if (!(process.env.HTTPS_PROXY || process.env.HTTP_PROXY)) return;
   const { ProxyAgent } = await import("undici");
   const dispatcher = new ProxyAgent(
     process.env.HTTPS_PROXY ?? process.env.HTTP_PROXY,
   );
-  return { dispatcher };
+  return dispatcher;
 };
 
 /** ポケモンの取得 */
 export const findPokemon = async (name) => {
-  const pokemon = await ofetch(
-    `https://pokeapi.co/api/v2/pokemon/${name}`,
-    await agentFactory(),
-  );
+  const pokemon = await ofetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
+    dispatcher: await createProxyAgent(),
+  });
   return pokemon;
 };
