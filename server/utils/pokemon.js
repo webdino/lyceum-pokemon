5e43fae2c74d59f60a7f1e64d1ba704011af048a
Kimiaki Kuno <knokmki612@gmail.com>
2025-02-05 16:06:24 +0900

    ofetch でのプロキシーエージェントの追加方法の見直し (#173)
    ofetch の agent オプションは Node v18 未満を対象に提供されているみたい
    Undici Dispatcher API を使用する形に見直します
    
    see https://github.com/unjs/ofetch/blob/main/README.md#%EF%B8%8F-adding-https-agent

--- server/utils/pokemon.js
+++ server/utils/pokemon.js
@@ -1,11 +1,14 @@
-import { ProxyAgent } from "proxy-agent";
 import { ofetch } from "ofetch";
+import { EnvHttpProxyAgent } from "undici";
 
-const agent = new ProxyAgent();
+// NOTE: Warning: EnvHttpProxyAgent is experimental, expect them to change at any time
+// See https://undici.nodejs.org/#/docs/api/EnvHttpProxyAgent.md
+const envHttpProxyAgent = new EnvHttpProxyAgent();
+
 /** ポケモンの取得 */
 export const findPokemon = async (name) => {
   const pokemon = await ofetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-    agent,
+    dispatcher: envHttpProxyAgent,
   });
   return pokemon;
 };
