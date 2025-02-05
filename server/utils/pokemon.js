faa42f29b08f4ed319cd299615159636e0d33256
Kimiaki Kuno <knokmki612@gmail.com>
2025-02-05 15:43:49 +0900

    use EnvHttpProxyAgent

--- server/utils/pokemon.js
+++ server/utils/pokemon.js
@@ -1,18 +1,12 @@
 import { ofetch } from "ofetch";
+import { EnvHttpProxyAgent } from "undici";
 
-const createProxyAgent = async () => {
-  if (!(process.env.HTTPS_PROXY || process.env.HTTP_PROXY)) return;
-  const { ProxyAgent } = await import("undici");
-  const dispatcher = new ProxyAgent(
-    process.env.HTTPS_PROXY ?? process.env.HTTP_PROXY,
-  );
-  return dispatcher;
-};
+const envHttpProxyAgent = new EnvHttpProxyAgent();
 
 /** ポケモンの取得 */
 export const findPokemon = async (name) => {
   const pokemon = await ofetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-    dispatcher: await createProxyAgent(),
+    dispatcher: envHttpProxyAgent,
   });
   return pokemon;
 };
